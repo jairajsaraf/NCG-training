261bfdcaab7008751ca6f02d36715f29c09378f3
jairajsaraf <jairaj.saraf01@gmail.com>
2022-09-01 12:00:05 +0530

    Create RandomArrayList.java

---
+++ RandomArrayList.java
@@ -0,0 +1,26 @@
+package com.example.training;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+
+public class RandomArrayList {
+
+    public static void main(String[] args) {
+        ArrayList<String> random = new ArrayList<>();
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter a string : ");
+        while (true){
+            String str = s.nextLine();
+
+            if("stop".equalsIgnoreCase(str)){
+                break;
+            }
+            random.add(str);
+            Collections.shuffle(random);
+        }
+        System.out.println("Randomised List : \n" + random);
+//        System.out.printf("Randomised Strings : \n%s", String.join("\n", random));
+    }
+}
