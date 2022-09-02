2414f79f021a018ee6b71ae73696c69e5e4e4930
jairajsaraf <jairaj.saraf01@gmail.com>
2022-09-02 13:00:35 +0530

    Update Tight_coupling.java

--- Tight_coupling.java
+++ Tight_coupling.java
@@ -1,18 +1,20 @@
 package com.example.training;
 //Tightly coupled classes
-class Volume
-{
-    public static void main(String args[])
-    {
-        Box b = new Box(5,5,5);
-        System.out.println(b.volume);
-    }
-}
+
 class Box
 {
     public int volume;
     Box(int length, int width, int height)
     {
         this.volume = length * width * height;
+    }
+}
+class Volume
+{
+    public static void main(String[] args)
+    {
+        Box b = new Box(5,5,5);
+
+        System.out.println(b.volume);
     }
 }
